b55b0e9acc5b07d234c17c517d8d9bbb32ffb19c
lxanas <912525577@qq.com>
2020-09-29 10:52:52 +0800

    完成作业3-3

---
+++ src/homework/homework3_3.java
@@ -0,0 +1,17 @@
+package homework;
+
+import javax.swing.*;
+
+public class homework3_3 {
+    public static void main(String[] args)
+    {
+        String str = JOptionPane.showInputDialog("输入数字，用分号分隔");
+        String[] num=str.split(";");
+        int number1=Integer.parseInt(num[0]);
+        int number2=Integer.parseInt(num[1]);
+        JOptionPane.showMessageDialog(null,"The sum is: " +(number1+number2)
+                +"\n"+"The difference is: "+(number1-number2)
+                +"\n"+"The product is:  "+(number1*number2)
+                +"\n"+"The division is: "+((float)number1/(float)number2));
+    }
+}
