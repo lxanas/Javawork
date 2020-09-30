fc9952b1264563cc8dbbdbfeeb4725afbeea3106
kyle <912525577@qq.com>
2020-09-30 21:40:22 +0800

    修正3-4代码

--- src/homework/homework3_4.java
+++ src/homework/homework3_4.java
@@ -9,15 +9,16 @@ public class homework3_4
         System.out.println("请输入Unicode字符对应的十六进制数字");
         Scanner in = new Scanner(System.in);
         String hex_num = in.nextLine();
-        char dec_num= (char) Long.parseLong(hex_num,16);
+        char dec_num = (char) Long.parseLong(hex_num, 16);
         System.out.println("以下为你的Unicode字符");
         System.out.println(dec_num);
         System.out.println("三秒后输出全部Unicode字符");
-        try {
+        try
+        {
             Thread.sleep(3000);
 
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
+        } catch (InterruptedException e)
+        {
             e.printStackTrace();
         }
         char c = '\u0000';
