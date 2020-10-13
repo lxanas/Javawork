9e053a2859ba6eb673e9f4ef758587a8552645da
lxanas <912525577@qq.com>
2020-10-13 09:18:38 +0800

    commit

--- src/Hello/helloworld.java
+++ src/Hello/helloworld.java
@@ -2,14 +2,16 @@ package Hello;
 
 import javax.swing.*;
 
+import static javax.swing.JOptionPane.*;
+
 public class helloworld {
     public static void main(String[] args)
     {
-        String str = JOptionPane.showInputDialog("输入数字，用分号分隔");
+        String str = showInputDialog("输入数字，用分号分隔");
         String[] num=str.split(";");
         int number1=Integer.parseInt(num[0]);
         int number2=Integer.parseInt(num[1]);
-        JOptionPane.showMessageDialog(null,"The sum is: " +(number1+number2)
+        showMessageDialog(null,"The sum is: " +(number1+number2)
                 +"\n"+"The difference is: "+(number1-number2)
                 +"\n"+"The product is:  "+(number1*number2)
                 +"\n"+"The division is: "+((float)number1/(float)number2));
